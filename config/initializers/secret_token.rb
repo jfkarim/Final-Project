d8f72adce25a8554f59b1749dd13055baf776e57
Jayan Fazal Karim <jfk303@nyu.edu>
2013-10-08 08:41:04 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrbanCanvas::Application.config.secret_token = '0ce97c4c6b037f5b6fa13689b991ea8debb1cc7b66b496183817437983589c9a696ce36d72ad4fff205a8d681daa80603cb07bbaee9741c480779645a30984dc'
